52b1a124bb3696a1a3bf994db9508196606bc39b
Siddarth Pai <siddarthpaim@live.com>
2021-03-18 23:36:12 +0530

    Changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './prognosis.png';
 import './App.css';
 
 function App() {
@@ -12,6 +12,68 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          
+      <div>
+        <h1>                    Medical Prognosis </h1>
+        <img src={logo} alt="Prognosis"   />
+        <h2>Preventing Heart Attack and other serious medical conditions</h2>
+      
+
+
+        <form>
+          <p>
+            <label>
+              Name  (as per records)
+    <input type="text" name="name" />
+
+            </label>
+          </p>
+          <p>
+          <label>
+            Age  (in Years)
+            <input type="text" name="Age" />
+            
+          </label>
+          </p>
+          <p>
+          <label>
+            Blood Pressure (BP)
+          <input type="text" name="BP" />
+
+          </label>
+          </p>
+          <p>
+          <label>
+         Blood Oxygen Level (SPO2)
+         </label>
+          <input type="text" name="SPO2" />
+          </p>
+          <p>
+          <label>
+          Electrocardiography (ECG)
+          <input type="text" name="ECG" />
+         
+          </label>
+          </p>
+          <p>
+          <label>
+            Heart Rate (in bpm)
+          <input type="text" name="Heart Rate" />
+          </label>
+          </p>
+          <p>
+          <label>
+            Submit
+          <input type="submit" value="submit" />
+          </label>
+          </p>
+          
+
+        </form>
+        <p>Developed by Siddarth Pai</p>
+      </div>
+
+      
          Welcome to Medical Prognosis App
         </a>
       </header>
